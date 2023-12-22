fbf6bc2c4e5b81ceba6ef050c1601e1716125754
allenck <allenck@localhost>
2023-12-21 19:22:50 -0500

    Created DpApiConfig and configuration YAML file - not tested.

--- src/main/java/com/ospreydcs/dp/api/config/DpApiConfig.java
+++ src/main/java/com/ospreydcs/dp/api/config/DpApiConfig.java
@@ -31,26 +31,49 @@ import java.io.FileNotFoundException;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.yaml.snakeyaml.Yaml;
 
+import com.ospreydcs.dp.api.config.grpc.GrpcConnectionConfig;
 import com.ospreydcs.dp.api.config.model.AEnvOverride;
 import com.ospreydcs.dp.api.config.model.EnvOverrideUtility;
 import com.ospreydcs.dp.api.config.model.YamlLoader;
 
 /**
- *
+ * <p>
+ * Active structure class containing default configuration parameters for Java-based
+ * Data Platform API libraries.
+ * </p>
+ * <p>
+ * The class maintains a singleton instance containing all the default configuration parameters
+ * as attributes.  The instance is created and populated upon first access using method 
+ * {@link #getInstance()}.
+ * </p>
+ * <p>
+ * <h2>NOTES:</h2>
+ * <ul>
+ * <li><b>WARNING</b> - all attributes have public accessibility.  Modififying attribute values
+ *     affects all systems using these parameters.
+ * </li>
+ * <li>Default configuration values are contained in the YAML file indicated by {@link #STR_CFG_FILE}.
+ * </li>
+ * <li>Values within the YAML file can be overridden by supported environment variables as marked by
+ *     the <code>AEnvOverride</code> annotation.
+ * </li>
+ * </ul> 
+ * 
  * @author Christopher K. Allen
  * @since Dec 21, 2023
  *
  */
 @AEnvOverride
-public class DpApiConfig {
+public final class DpApiConfig {
     
     //
     // Application Resources
     //
     
     /** Location of the application properties file (relative path and name) */
-    public static final String      STR_CFG_FILE = "application.yml";
+    public static final String      STR_CFG_FILE = "dp-api-config.yml";
     
     
     // 
@@ -109,8 +132,124 @@ public class DpApiConfig {
     
 
     //
-    // Properties
+    // Configuration Parameters
     //
     
+    /** Data Platform data archive parameters */
+    @AEnvOverride.Struct
+    public Archive  archive;
     
+    /** Data Platform services connection parameters */
+    @AEnvOverride.Struct
+    public Services services;
+    
+    /**
+     * Structure containing Data Platform archive parameters.
+     *
+     */
+    public static final class Archive {
+        
+        /** Archive inception date (earliest possible timestamp) */
+        @AEnvOverride.Field(env="DP_ARCHIVE_INCEPTION")
+        public String   inception;
+
+        
+        // 
+        // Object Overrides
+        //
+        
+        /**
+         *
+         * @see @see java.lang.Object#equals(java.lang.Object)
+         */
+        @Override
+        public boolean equals(Object obj) {
+            
+            // Cast comparison object
+            Archive arc;
+            if (obj instanceof Archive)
+                arc = (Archive)obj;
+            else
+                return false;
+            
+            // Check equivalence
+            return arc.inception.equals(this.inception);
+        }
+    }
+    
+    /**
+     * Structure containing connection parameters for Data Platform services.
+     *
+     * @see GrpcConnectionConfig
+     */
+    public static final class Services {
+        
+        /** Data Platform Ingestion Service connection parameters */
+        @AEnvOverride.Struct
+        public GrpcConnectionConfig     ingestion;
+        
+        /** Data Platform Query Service connection parameters */
+        @AEnvOverride.Struct
+        public GrpcConnectionConfig     query;
+
+        
+        //
+        // Object Overrides
+        //
+        
+        /**
+         *
+         * @see @see java.lang.Object#equals(java.lang.Object)
+         */
+        @Override
+        public boolean equals(Object obj) {
+            
+            // Cast comparison object
+            Services    srvs;
+            if (obj instanceof Services)
+                srvs = (Services)obj;
+            else
+                return false;
+            
+            // Check equivalence
+            return srvs.ingestion.equals(this.ingestion) 
+                    && srvs.query.equals(this.query);
+        }
+    }
+    
+    
+    //
+    // Object Overrides
+    //
+
+    /**
+     *
+     * @see @see java.lang.Object#equals(java.lang.Object)
+     */
+    @Override
+    public boolean equals(Object obj) {
+        
+        // Cast comparison object
+        DpApiConfig cfg;
+        if (obj instanceof DpApiConfig)
+            cfg = (DpApiConfig)obj;
+        else
+            return false;
+        
+        // Check equivalence
+        return cfg.archive.equals(this.archive) &&
+                cfg.services.equals(this.services);
+    }
+
+    /**
+     *
+     * @see @see java.lang.Object#toString()
+     */
+    @Override
+    public String toString() {
+        Yaml    yml = new Yaml();
+        String  str = yml.dump(this);
+        
+        return str;
+    }
 }
